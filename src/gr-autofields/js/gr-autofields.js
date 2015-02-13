c95c798e9463fa4abc15d983706d63990d4559b9
Miller Barros <millerbarros@hotmail.com>
2015-02-13 13:42:41 -0200

    Correção no módulo

--- src/gr-autofields/js/gr-autofields.js
+++ src/gr-autofields/js/gr-autofields.js
@@ -141,125 +141,14 @@
             }
         }])
         .config(['$provide', '$autofieldsProvider', function($provide, $autofieldsProvider){
-            
             var makeTa = function(){
-            
-                var imgOnSelectAction = function(event, $element, editorScope){
-                    // setup the editor toolbar
-                    // Credit to the work at http://hackerwins.github.io/summernote/ for this editbar logic/display
-                    var finishEdit = function(){
-                        editorScope.updateTaBindtaTextElement();
-                        editorScope.hidePopover();
-                    };
-                    event.preventDefault();
-                    editorScope.displayElements.popover.css('width', '375px');
-                    var container = editorScope.displayElements.popoverContainer;
-                    container.empty();
-                    var buttonGroup = angular.element('<div class="btn-group" style="padding-right: 6px;">');
-                    var fullButton = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" unselectable="on" tabindex="-1">100% </button>');
-                    fullButton.on('click', function(event){
-                        event.preventDefault();
-                        $element.css({
-                            'width': '100%',
-                            'height': ''
-                        });
-                        finishEdit();
-                    });
-                    var halfButton = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" unselectable="on" tabindex="-1">50% </button>');
-                    halfButton.on('click', function(event){
-                        event.preventDefault();
-                        $element.css({
-                            'width': '50%',
-                            'height': ''
-                        });
-                        finishEdit();
-                    });
-                    var quartButton = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" unselectable="on" tabindex="-1">25% </button>');
-                    quartButton.on('click', function(event){
-                        event.preventDefault();
-                        $element.css({
-                            'width': '25%',
-                            'height': ''
-                        });
-                        finishEdit();
-                    });
-                    var resetButton = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" unselectable="on" tabindex="-1">Reset</button>');
-                    resetButton.on('click', function(event){
-                        event.preventDefault();
-                        $element.css({
-                            width: '',
-                            height: ''
-                        });
-                        finishEdit();
-                    });
-                    buttonGroup.append(fullButton);
-                    buttonGroup.append(halfButton);
-                    buttonGroup.append(quartButton);
-                    buttonGroup.append(resetButton);
-                    container.append(buttonGroup);
-
-                    buttonGroup = angular.element('<div class="btn-group" style="padding-right: 6px;">');
-                    var floatLeft = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" unselectable="on" tabindex="-1"><i class="fa fa-align-left"></i></button>');
-                    floatLeft.on('click', function(event){
-                        event.preventDefault();
-                        // webkit
-                        $element.css('float', 'left');
-                        // firefox
-                        $element.css('cssFloat', 'left');
-                        // IE < 8
-                        $element.css('styleFloat', 'left');
-                        finishEdit();
-                    });
-                    var floatRight = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" unselectable="on" tabindex="-1"><i class="fa fa-align-right"></i></button>');
-                    floatRight.on('click', function(event){
-                        event.preventDefault();
-                        // webkit
-                        $element.css('float', 'right');
-                        // firefox
-                        $element.css('cssFloat', 'right');
-                        // IE < 8
-                        $element.css('styleFloat', 'right');
-                        finishEdit();
-                    });
-                    var floatNone = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" unselectable="on" tabindex="-1"><i class="fa fa-align-justify"></i></button>');
-                    floatNone.on('click', function(event){
-                        event.preventDefault();
-                        // webkit
-                        $element.css('float', '');
-                        // firefox
-                        $element.css('cssFloat', '');
-                        // IE < 8
-                        $element.css('styleFloat', '');
-                        finishEdit();
-                    });
-                    buttonGroup.append(floatLeft);
-                    buttonGroup.append(floatNone);
-                    buttonGroup.append(floatRight);
-                    container.append(buttonGroup);
-
-                    buttonGroup = angular.element('<div class="btn-group">');
-                    var remove = angular.element('<button type="button" class="btn btn-default btn-sm btn-small" unselectable="on" tabindex="-1"><i class="fa fa-trash-o"></i></button>');
-                    remove.on('click', function(event){
-                        event.preventDefault();
-                        $element.remove();
-                        finishEdit();
-                    });
-                    buttonGroup.append(remove);
-                    container.append(buttonGroup);
-
-                    editorScope.showPopover($element);
-                    editorScope.showResizeOverlay($element);
-                };
-
                 $provide.decorator('taOptions', ['taRegisterTool', '$delegate', function(taRegisterTool, taOptions){
-
                     taOptions.toolbar = [
                         ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
                         ['bold', 'italics', 'underline', 'ul', 'ol', 'redo', 'undo', 'clear'],
                         ['justifyLeft','justifyCenter','justifyRight','indent','outdent'],
                         ['html', 'insertImage', 'insertVideo', 'insertLink']
                     ];
-
                     taOptions.classes = {
                         focussed: 'focussed',
                         toolbar: 'btn-toolbar',
@@ -270,24 +159,16 @@
                         textEditor: 'form-control',
                         htmlEditor: 'form-control'
                     };
-
                     return taOptions;
-
                 }]);
                 
             };
-            
             $autofieldsProvider.registerHandler('html', function(directive, field, index){
                 var fieldElements = $autofieldsProvider.field(directive, field, '<text-angular/>');
-                
                 fieldElements.fieldContainer.append(toolbar);
-
                 fieldElements.fieldContainer.append(fieldElements.input);
-                
                 fieldElements.input.removeClass('form-control');
-                
                 makeTa();
-
                 return fieldElements.fieldContainer;
             });
         }]);
--- src/gr-filemanager/views/delete-files.php
+++
@@ -1,6 +0,0 @@
-<small>
-    Tem certeza que deseja excluír os arquivos abaixo?<br/>
-    <ul>
-        <li ng-repeat="file in files">{{file.basename}}</li>
-    </ul>
-</small>
