9acf186710c7b052c496ff17ddad9f59974765d6
Miller Barros <millerbarros@hotmail.com>
2015-02-13 11:15:35 -0200

    Update modules

---
+++ src/gr-filemanager/view/delete-files.php
@@ -0,0 +1,6 @@
+<small>
+    Tem certeza que deseja excluír os arquivos abaixo?<br/>
+    <ul>
+        <li ng-repeat="file in files">{{file.basename}}</li>
+    </ul>
+</small>
---
+++ src/gr-filemanager/views/add-folder.php
@@ -0,0 +1,2 @@
+<small>Nome da nova pasta</small>
+<input type="text" name="path" class="form-control" ng-model="path" placeholder="Nome da nova pasta" autofocus/>
